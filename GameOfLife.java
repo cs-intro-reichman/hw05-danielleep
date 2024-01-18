ce1b39239a43f33500322baed99313e3c3b98fa0
danielleep <113351600+danielleep@users.noreply.github.com>
2024-01-18 16:54:15 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -184,7 +184,7 @@ public class GameOfLife
 		{
 			for(int j=0; j<arr[0].length;j++)
 			{
-				System.out.print(arr[i][j]+"   ");
+				System.out.printf("%3s",arr[i][j]);
 			}
 			System.out.println();
 		}
